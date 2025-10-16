0f0f1212b36cba108a70b4c17e03201570d824ed
Rob <rob.sakal23@gmail.com>
2025-10-16 16:12:13 -0400

    filter working

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
-function renderBooks () {
+function renderBooks(filter) {
   const booksWrapper = document.querySelector('.books');
 
   const books = getBooks();
 
-  booksWrapper.innerHTML =
-  `<div class="book">
+  
+  if (filter === 'LOW_TO_HIGH') {
+     console.log(filter)
+     books.sort((a, b) => a.originalPrice - b.originalPrice)
+    }
+    else if (filter === 'HIGH_TO_LOW') {
+      books.sort((a, b) => b.originalPrice - a.originalPrice)
+    }
+    else if (filter === 'RATING') {
+      books.sort((a, b) => b.rating - a.rating)
+    }
+
+const booksHtml = books.map((book) => {
+return `<div class="book">
     <figure class="book__img--wrapper">
-      <img class="book__img" src="${books[0].url}" alt="">
+      <img class="book__img" src="${book.url}" alt="">
     </figure>
     <div class="book__title">
-      Crack the Coding Interview
+      ${book.title}
     </div>
     <div class="book__ratings">
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star"></i>
-      <i class="fas fa-star-half-alt"></i>
+     ${ratingsHTML(book.rating)}
     </div>
     <div class="book__price">
-      <span class="book__price--normal">$59.95</span> $14.95
+      <span>$${book.originalPrice.toFixed(2)}</span> 
     </div>
-  </div>`
+  </div>`;
+}).join('');
+
+ booksWrapper.innerHTML = booksHtml;
+}
+
+function ratingsHTML(rating) {
+  let ratingHTML = "";
+   for (let i = 0; i < Math.floor(rating); ++i) {
+   ratingHTML += '<i class="fas fa-star"></i>\n';
+ }
+  if (!Number.isInteger(rating)) {
+   ratingHTML += '<i class="fas fa-star-half-alt"></i>\n'
+ }
+ return ratingHTML;
+}
+
+function filterBooks(event) {
+    renderBooks(event.target.value);
 }
 
 setTimeout(() => {
